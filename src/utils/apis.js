a085ac35fb95decd68fed1de2979894bb9c8a4d3
Aziz Fatih Fauzi <92005833+AzizFauzi99@users.noreply.github.com>
2023-05-10 00:41:51 +0700

    update apis

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,6 +1,6 @@
 // const BASE_URL = 'https://api.findidealworker.my.id';
 // const BASE_URL = 'http://34.101.86.80';
-const BASE_URL = 'https://backend-dot-quantum-device-12345.uc.r.appspot.com/';
+const BASE_URL = 'https://20230507t170313-dot-faisal-project-380209.et.r.appspot.com/';
 
 function getAccessToken() {
   return localStorage.getItem('accessToken');
